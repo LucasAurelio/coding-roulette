b29b3ae9d7f20ee84016b6729b4141c33860f164
Lucas Lima <lucas.aurelio.lima@gmail.com>
2022-05-30 14:11:08 -0300

    Initial version of the roulette
    Basic python code to randomly chose the daily task

---
+++ roulette.py
@@ -0,0 +1,8 @@
+import random
+
+
+currentProjects = ['Coding Roulette','Recipies Book', 'Spend-less', 'Heartstopper Quote Generator']
+
+dailyChoice = random.choice(currentProjects)
+
+print("Today you're going to work on the "+ dailyChoice+ " project")
